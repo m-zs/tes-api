2292743c2dcaadb288da83b8f17a1ea84e1697eb
M. Zasada <mzasada93@gmail.com>
2025-07-03 20:09:52 +0200

    feat: add linting setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,8 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import eslintPluginImport from 'eslint-plugin-import';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
 
 export default tseslint.config(
   {
@@ -29,7 +31,54 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+    },
+  },
+  {
+    plugins: {
+      import: eslintPluginImport,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: createTypeScriptImportResolver({
+          alwaysTryTypes: true,
+        }),
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // Node.js built-in modules
+            'external', // npm packages
+            'internal', // Absolute imports (e.g., @/utils)
+            ['parent', 'sibling', 'index'], // Relative imports
+            'object', // `import * as _ from 'lodash'`
+            'type', // Type-only imports
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'import/no-unresolved': 'error',
+      'import/no-duplicates': 'error',
+      'import/no-cycle': 'warn',
+      '@typescript-eslint/unbound-method': 'off',
     },
   },
 );
